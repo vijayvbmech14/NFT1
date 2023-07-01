eaf94eeda33a8006d92495a1715e1aa3d0d40b3e
vijayvbmech14 <vijayvbmech14@gmail.com>
2023-07-01 14:41:42 +0530

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,26 @@
+/**
+* @type import('hardhat/config').HardhatUserConfig
+*/
+require("@nomiclabs/hardhat-ethers");
+require('dotenv').config();
+const { PRIVATE_KEY } = process.env;
+module.exports = {
+  defaultNetwork: "PolygonMumbai",
+  networks: {
+    hardhat: {
+    },
+     PolygonMumbai: {
+      url: process.env.NFT_STORAGE_API_KEY,
+      accounts: [PRIVATE_KEY]
+    }
+  },
+  solidity: {
+    version: "0.8.12",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+}
